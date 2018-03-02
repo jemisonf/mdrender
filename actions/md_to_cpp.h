554e273c13cb7a082960467d987b3e065f6cde37
Fischer Jemison <jemisonf@oregonstate.edu>
2018-03-01 17:07:04 -0800

    Adds interfaces to header files (#5)

--- actions/md_to_cpp.h
+++ actions/md_to_cpp.h
@@ -1,8 +1,15 @@
 #ifndef MD_TO_CPP_H
 #define MD_TO_CPP_H
 
-class MdToCpp {
+#include "../filedata/md_data.h"
+#include <iostream>
+#include <fstream>
 
+class MdToCpp {
+  public:
+    MdToCpp();
+    void set_filestream(const std::ifstream &);
+    const MdData & get_md_data();
 }
 
 #endif
