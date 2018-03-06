14185c50a63796381b302a1b7c5f524e0d67a8bf
Fischer Jemison <jemisonf@oregonstate.edu>
2018-03-05 19:36:16 -0800

    Md to cpp (#16)
    * adds parse_line functionality
    
    * Adds md to cpp test

--- actions/md_to_cpp.cpp
+++ actions/md_to_cpp.cpp
@@ -4,18 +4,42 @@ MdToCpp::MdToCpp() {
 
 }
 
-void MdToCpp::set_filestream(const std::ifstream &input_stream) {
-  this->file_stream = input_stream;
+void MdToCpp::set_file(const std::string filename) {
+  this->file_stream.open(filename.c_str());
 }
 
-const MdData & MdToCpp::get_md_data() {
+const MdData MdToCpp::get_md_data() {
+  MdData data;
   // initialize new MdData
   // for each line in file_stream
     // call parse_line
     // add the result to MdData
   // return MdData
+  return data;
 }
 
 TextNode MdToCpp::parse_line(const std::string line) {
+  int text_start;
+  std::string type, text = "";
+  if (line[0] == '#') {
+    while (line[text_start] == '#') {
+      text_start++;
+    }
+    type = "h";
+    type += (char) text_start + 49;
+    text = line.substr(text_start, line.length() - text_start);
+  } else if (line[0] > '0' || line[0] <= '9') {
+    type = "ol";
+    text = line.substr(3, line.length() - 3);
+  } else if (line[0] == '-' || line[0] == '*' || line[0] == '+') {
+    type = "ul";
+    text = line.substr(2, line.length() - 2);
+  } else {
+    type = "p";
+    text = line;
+  }
+  return TextNode(text, type);
+    
+
    
 }
