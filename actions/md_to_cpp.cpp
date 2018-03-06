ddba655fc5dd8a87f745fbaf0600e275e4c5c170
Fischer Jemison <jemisonf@oregonstate.edu>
2018-03-05 16:43:06 -0800

    adds parse_line functionality

--- actions/md_to_cpp.cpp
+++ actions/md_to_cpp.cpp
@@ -17,5 +17,28 @@ const MdData & MdToCpp::get_md_data() {
 }
 
 TextNode MdToCpp::parse_line(const std::string line) {
+  char temp = line[0]
+  int text_start;
+  std::string type, text = '';
+  if (line[0] == '#') {
+    while (line[text_start] == '#') {
+      text_start++;
+    }
+    type = "h";
+    type += (char) text_start + 49;
+    text = line.substr(text_start, line.length() - text_start);
+  } else if (line[0] > '0' || line[0] <= '9') {
+    type = "ol";
+    text = line.substr(3, line.length() - 3);
+  } else if (line[0] == '-' || line[0] == '*' || line[0] == '+') {
+    type = "ul";
+    text = line.substr(2, line.length() - 2);
+  } else {
+    type = "p";
+    text = line;
+  }
+  return TextNode(text, type);
+    
+
    
 }
