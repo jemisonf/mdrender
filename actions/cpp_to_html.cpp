a4e37958755b6ef60b5cb41845c4d161fbfdfa70
Sean <gillens@oregonstate.edu>
2018-03-15 15:30:58 -0700

    Make (#32)
    * attempted lists... <ol>/<ul> not appearing
    
    * add list open/close

--- actions/cpp_to_html.cpp
+++ actions/cpp_to_html.cpp
@@ -15,6 +15,7 @@ const std::string CppToHtml::get_html(){
     TextNode cur_node = data.dequeue_node();
     std::string cur_node_type = cur_node.get_type();
     std::string cur_node_text = cur_node.get_text();
+    std::string last_node_type = "";
     //header
     if (cur_node_type[0] == 'h'){
       std::string tag = "h";
@@ -26,7 +27,32 @@ const std::string CppToHtml::get_html(){
       line += "<p>" + cur_node_text + "</p>\n";
     }
 
-    
+    else if (cur_node_type[0] == 'o') {
+/*
+      if (last_node_type[0] != 'o') {
+         std::string hold = "<ol>" + '\n';
+         html += hold;
+      }
+*/
+      if(html[html.length() - 3] != 'i'){
+        line += "<ol>\n";
+      }
+      line += "<li>" + cur_node_text + "</li>\n";
+      if(data.front_node().get_type() != "ol"){
+        line += "</ol>\n";
+      }     
+    }
+
+    else if (cur_node_type[0] == 'u') {
+      if(html[html.length() - 3] != 'i'){
+        line += "<ul>\n";
+      }
+      line += "<li>" + cur_node_text + "</li>\n";
+      if(data.front_node().get_type() != "ul"){
+        line += "</ul>\n";
+      }
+    }
+
     std::size_t found = line.find("**");
     if (found!=std::string::npos) {
       line.replace(line.begin()+found, line.begin()+found+2,"<b>");
@@ -63,7 +89,9 @@ const std::string CppToHtml::get_html(){
       line.replace(line.begin()+found,line.begin()+found+2,"</del>");
     }
 
+    last_node_type = cur_node_type;
     html += line;
+
   }
 
   return this->html;
